d327f7a330e6783a50a2d6876b3e3959cf7fb490
Ervina Nanda Fitrianova <88192807+ervinananda@users.noreply.github.com>
2021-11-11 00:28:34 +0700

    Create user.java

---
+++ user.java
@@ -0,0 +1,27 @@
+package Lelang;
+
+public class User {
+    void setNama(String nama){
+        System.out.println(nama);
+    }
+
+    void setAlamat(String Alamat){
+        System.out.println(Alamat);
+    }
+
+    void setTelepon(String telp){
+        System.out.println(telp);
+    }
+
+    String getNama(int ID){
+        return null;
+    }
+
+    String getAlamat(int ID){
+        return null;
+    }
+
+    String getTelepon(int ID){
+        return null;
+    }
+}
