5dbe4323658f4a79064ce6ddf0ba6c663b1b5674
Ervina Nanda Fitrianova <88192807+ervinananda@users.noreply.github.com>
2021-11-11 00:27:24 +0700

    Create masyarakat.java

---
+++ masyarakat.java
@@ -0,0 +1,58 @@
+package Lelang;
+
+import java.util.ArrayList;
+
+public class Masyarakat extends User{
+    private ArrayList<String> namaMasyarakat = new ArrayList<String>();
+    private ArrayList<String> alamat = new ArrayList<String>();
+    private ArrayList<String> telepon = new ArrayList<String>();
+    private ArrayList<Integer> idMasyarakat = new ArrayList<Integer>();
+
+    public void Masyarakat(String nama, String alamat, String telp){
+        setNama(nama);
+        setAlamat(alamat);
+        setTelepon(telp);
+        setIdMasyarakat(namaMasyarakat.indexOf(nama));
+    }
+
+    public int getIndexFromName(String nama){
+        return namaMasyarakat.indexOf(nama);
+    }
+    public int getIdMasyarakat(int id) {
+        return idMasyarakat.get(id);
+    }
+
+    public void setIdMasyarakat(int id) {
+        this.idMasyarakat.add(id);
+    }
+
+    @Override
+    public void setNama(String nama) {
+        this.namaMasyarakat.add(nama);
+    }
+
+    @Override
+    public void setAlamat(String Alamat) {
+        this.alamat.add(Alamat);
+    }
+
+    @Override
+    public void setTelepon(String telp) {
+        this.telepon.add(telp);
+    }
+
+    @Override
+    public String getNama(int ID) {
+        return namaMasyarakat.get(ID);
+    }
+
+    @Override
+    public String getAlamat(int ID) {
+        return alamat.get(ID);
+    }
+
+    @Override
+    public String getTelepon(int ID) {
+        return alamat.get(ID);
+    }
+}
