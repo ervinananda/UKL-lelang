c8658e97905e721bafbfeb0c3546e03d9ed328fb
Ervina Nanda Fitrianova <88192807+ervinananda@users.noreply.github.com>
2021-11-11 00:26:10 +0700

    Create lelang.java

---
+++ lelang.java
@@ -0,0 +1,46 @@
+package Lelang;
+
+import java.util.ArrayList;
+
+public class Lelang implements mengLelang{
+    ArrayList<Boolean> Status = new ArrayList<Boolean>();
+    ArrayList<Integer> idPenawar = new ArrayList<>();
+    ArrayList<Integer> idBarang = new ArrayList<>();
+    ArrayList<Integer> hargaTawar = new ArrayList<>();
+
+
+    @Override
+    public void addPenawar(int ID) {
+        this.idPenawar.add(ID);
+    }
+
+    @Override
+    public void addIdBarang(int ID) {
+        this.idBarang.add(ID);
+    }
+
+    @Override
+    public void addHargaTawar(int harga) {
+        this.hargaTawar.add(harga);
+    }
+
+    @Override
+    public void setStatus(boolean status) {
+        this.Status.add(status);
+    }
+
+    @Override
+    public int getHargaTertinggi() {
+        return 0;
+    }
+
+    @Override
+    public int getIdPenawar() {
+        return 0;
+    }
+
+    @Override
+    public boolean getStatus(int id) {
+        return this.Status.get(id);
+    }
+}
